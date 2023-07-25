c14456123826648d37bded6044c1d23625147533
G G <gustavs.petris98@gmail.com>
2023-07-25 17:46:18 +0300

    Rock-Paper-Scissors-Lizard-Spock console game against single computer

---
+++ src/main/java/org/example/RockPaperScissorsLizardSpockMainApp.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class RockPaperScissorsLizardSpockMainApp {
+    public static void main(String[] args) {
+
+        GameAgainstOneComputer game = new GameAgainstOneComputer();
+        game.playGame();
+
+    }
+}
